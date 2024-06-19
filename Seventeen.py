a3235664e89806419719fc481921ead970f51b8f
Shambhavi Sinha <123668966+Samtoosoon@users.noreply.github.com>
2024-06-19 13:14:17 +0530

    Seventeen.py

---
+++ Seventeen.py
@@ -0,0 +1,4 @@
+# 17. Program to convert a string to title case
+user_input = input("Enter a string: ")
+title_case_string = user_input.title()
+print(f"The title case string is: {title_case_string}")
