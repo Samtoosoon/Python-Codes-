1d7a26f95f55aaebf97e8d500d074de81d8472c6
Shambhavi Sinha <123668966+Samtoosoon@users.noreply.github.com>
2024-06-19 13:21:41 +0530

    Twentysevenquestion.py

---
+++ Twentysevenquestion.py
@@ -0,0 +1,4 @@
+# 27. Program to convert a string into a list of its characters
+user_input = input("Enter a string: ")
+char_list = list(user_input)
+print(f"The list of characters is: {char_list}")
