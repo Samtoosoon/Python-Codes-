e97ec1621ad458427d10de4add1a5a16c83a777e
Shambhavi Sinha <123668966+Samtoosoon@users.noreply.github.com>
2024-06-19 13:16:23 +0530

    Twentyquestion.py

---
+++ Twentyquestion.py
@@ -0,0 +1,4 @@
+# 20. Program to sum a list of numbers
+numbers = list(map(float, input("Enter a list of numbers separated by spaces: ").split()))
+total_sum = sum(numbers)
+print(f"The sum of the numbers is {total_sum}")
