a1b3939d2b1ef83605ca5f5bbaee2152c72053f1
Shambhavi Sinha <123668966+Samtoosoon@users.noreply.github.com>
2024-06-19 13:03:43 +0530

    Eightquestion.py

---
+++ Eightquestion.py
@@ -0,0 +1,5 @@
+# Program to concatenate two strings
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+result = str1 + str2
+print(f"The concatenated string is: {result}")
