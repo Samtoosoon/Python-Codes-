b1d86f616f2a186f5cc03555e884cf68e84539b5
Shambhavi Sinha <123668966+Samtoosoon@users.noreply.github.com>
2024-06-19 13:07:36 +0530

    Eleventh.py

--- Eleventh.py
+++ Eleventh.py
@@ -5,3 +5,7 @@ def fibonacci(n):
         next_number = sequence[-1] + sequence[-2]
         sequence.append(next_number)
     return sequence[:n]
+
+n = int(input("Enter the number of Fibonacci numbers to generate: "))
+fibonacci_sequence = fibonacci(n)
+print(f"The first {n} numbers in the Fibonacci sequence are: {fibonacci_sequence}")
